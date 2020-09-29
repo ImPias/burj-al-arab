781f15ebf5b6bdedf70fcb55bf0853a32e5d5b10
impias <kazipias123@gmail.com>
2020-09-29 23:19:50 +0600

    Burj-Al-Arab-Module-44

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ95_l3fMB66J_IrkmF9JnnLKp60Ofnnw",
+    authDomain: "burj-al-arab-nongor-soft.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-nongor-soft.firebaseio.com",
+    projectId: "burj-al-arab-nongor-soft",
+    storageBucket: "burj-al-arab-nongor-soft.appspot.com",
+    messagingSenderId: "17738583094",
+    appId: "1:17738583094:web:d36e6168d00275e1e99c97"
+};
+export default firebaseConfig;
